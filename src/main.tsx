4b516c55df7312b82834ca33ee73444ab7ceff04
bye-shuvo <abuhasnatshuvo605@gmail.com>
2026-02-20 01:18:07 +0600

    Update the Main.tsx for React-Router for client side routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import router from './Router.tsx'
+import { RouterProvider } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <RouterProvider router={router} />
 )
